047e785d3b6d72c996c2de66e2e34a63d84b38fc
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-14 10:37:39 -0300

    Criação do category na camada Service

---
+++ src/services/categoryService.js
@@ -0,0 +1,11 @@
+const { Category } = require('../database/models');
+
+const create = async (name) => {
+  const category = await Category.create({ name });
+
+  return category;
+};
+
+module.exports = {
+  create,
+};

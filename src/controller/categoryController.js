fd59b528d237e1c1032ebc1ebee5483d95387d4d
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-14 10:55:54 -0300

    Criação do getAll no categoryService

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -8,6 +8,13 @@ const create = async (req, res) => {
   res.status(201).json(category);
 };
 
+const getAll = async (_req, res) => {
+  const categories = await CategoryService.getAll();
+
+  res.status(200).json(categories);
+};
+
 module.exports = {
   create,
+  getAll,
 };
